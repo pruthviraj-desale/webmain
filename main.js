3a5c13733b8ecb1ecca64b1328e6b584f6d883a8
Pruthviraj Desale <pruthvirajd007@gmail.com>
2024-02-23 19:16:35 +0530

    added buttons and divs

--- main.js
+++ main.js
@@ -1,70 +1,14 @@
-/*// Math.floor: Rounds down to the nearest integer
-console.log("Math.floor(3.7):", Math.floor(3.7));
-console.log("Math.floor(-3.7):", Math.floor(-3.7));
+const parent = document.querySelector('.parent')
+const child = document.querySelector('.child')
+const submitButton = document.querySelector('.submit')
 
-// Math.ceil: Rounds up to the nearest integer
-console.log("Math.ceil(3.2):", Math.ceil(3.2));
-console.log("Math.ceil(-3.2):", Math.ceil(-3.2));
+submitButton.addEventListener('click', ()=>{
+    child.style.backgroundColor = 'red';
+    console.log("child color changed to red")
 
-// Math.abs: Returns the absolute value of a number
-console.log("Math.abs(-42):", Math.abs(-42));
-
-// Math.random: Returns a random floating-point number between 0 (inclusive) and 1 (exclusive)
-console.log("Math.random():", Math.random());
-
-// To get a random integer between a specific range, you can use Math.floor with Math.random
-
-let randomInt = Math.floor(Math.random() * (10));
-console.log("Random integer between 1 and 10:", randomInt);*/
-
-
-// let str = "pruthviraj";
-// let len = str.length;
-// let randomInt = Math.floor(Math.random()*(len));
-// console.log(str.charAt(randomInt));
-
-// // Alert dialog
-// alert("This is an alert dialog!");
-
-// // Confirm dialog
-// let result = confirm("Do you want to proceed?");
-// if (result === true) {
-//     console.log("User clicked OK.");
-// } else {
-//     console.log("User clicked Cancel.");
-// }
-
-// // Prompt dialog
-// let name = prompt("Please enter your name:", "John Doe");
-// if (name !== null) {
-//     console.log("Hello, " + name + "!");
-// } else {
-//     console.log("User clicked Cancel.");
-// }
-
-
-// Function declaration
-function greet(name) {
-    console.log("Hello, " + name + "!");
-}
-
-// Function call
-greet("John");
-
-// Function expression
-let multiply = function(x, y) {
-    return x * y;
-};
-
-// Function call
-let result = multiply(3, 4);
-console.log("Result of multiplication:", result);
-
-// Arrow function
-let subtract = (a, b) => {
-    return a - b;
-};
-
-// Function call
-let difference = subtract(7, 2);
-console.log("Difference:", difference);
+    const newDiv = document.createElement('div')
+    newDiv.classList.add('child')
+    newDiv.innerHTML = "this is a new div"
+    parent.appendChild(newDiv)
+    console.log("new div added")
+})
